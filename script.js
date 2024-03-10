d7ec69460e0ff73f7b19c38c46678edb78db5f8e
sai neeraj reddy <104774846+Neeraj170@users.noreply.github.com>
2024-03-10 08:05:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function my(){
+            window.location.href="ordernow.html";
+        }
+function inc(){
+    window.location.href="inc.html";
+}
+
+function home(){
+    window.location.href="shop.html";
+}
+function fin(){
+    window.location.href="fin.html";
+}
+function inv(){
+    window.location.href="inv.html";
+}
+function home(){
+    window.location.href="shop.html";
+}
+function led(){
+    window.location.href="led.html";
+}
+function exp(){
+    window.location.href="exp.html";
+}
+function bud(){
+    window.location.href="bud.html";
+}
